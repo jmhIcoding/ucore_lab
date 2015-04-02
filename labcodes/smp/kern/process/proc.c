62d885bbe4a126fd6f27b99fbfbc556cd9f8e934
akovski <akovskiboy@gmail.com>
2015-04-02 14:06:11 +0800

    change gitignore

--- labcodes/smp/kern/process/proc.c
+++ labcodes/smp/kern/process/proc.c
@@ -994,8 +994,8 @@ init_main(void *arg) {
     if (pid <= 0) {
         panic("create user_main failed.\n");
     }
-    extern void check_sync(void);
-    check_sync();                // check philosopher sync problem
+    //extern void check_sync(void);
+    //check_sync();                // check philosopher sync problem
 
     while (do_wait(0, NULL) == 0) {
         schedule();
